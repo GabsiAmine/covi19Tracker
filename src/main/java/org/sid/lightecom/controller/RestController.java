de8011303ee18087fda90bf9481330fff4a20dca
GabsiAmine <ma.elgabsi@gmail.com>
2020-06-04 17:33:56 +0100

    adding a rest Controller for the angular app

--- src/main/java/org/sid/lightecom/controller/RestController.java
+++ src/main/java/org/sid/lightecom/controller/RestController.java
@@ -5,28 +5,27 @@ import java.util.List;
 import org.sid.lightecom.model.LocationState;
 import org.sid.lightecom.service.CoronaVisrusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class HomeController {
-
+@CrossOrigin("*")
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+	
 	@Autowired
 	private CoronaVisrusDataService coronaVisrusDataService;
 	
-	@GetMapping("/ContryList")
-	public String home(Model model) {
+	@GetMapping("/listofcases")
+	public List<LocationState> listofcases(LocationState locationState) {
 		
 		List<LocationState> allstats = coronaVisrusDataService.getAllstate();
 		
 		int totalReportedCases = allstats.stream().mapToInt(stat -> stat.getLatestTotal()).sum();
 		int totalNewCases = allstats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
 
-		model.addAttribute("locationstate",allstats);
-		model.addAttribute("totalReportedCases",totalReportedCases);
-		model.addAttribute("totalNewCases",totalNewCases);
 		
-		return "home";
+		return allstats;
 	}
+	
+
 }
